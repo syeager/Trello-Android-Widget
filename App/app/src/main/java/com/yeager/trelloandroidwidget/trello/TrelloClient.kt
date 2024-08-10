edb625c6ad142b8cc8a5a68a9745c691314aeeea
Steve Yeager <yeagz7@gmail.com>
2024-08-09 19:43:02 -0700

    Add endpoints

--- App/app/src/main/java/com/yeager/trelloandroidwidget/trello/TrelloClient.kt
+++ App/app/src/main/java/com/yeager/trelloandroidwidget/trello/TrelloClient.kt
@@ -1,9 +1,11 @@
 package com.yeager.trelloandroidwidget.trello
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
 import io.ktor.http.HttpHeaders
 import io.ktor.http.URLProtocol
 import io.ktor.http.headers
@@ -34,4 +36,30 @@ class TrelloClient(key: String, token: String) {
             }
         }
     }
+
+    private suspend inline fun <reified T> getList(
+        route: String,
+        vararg params: Pair<String, String>
+    ): Array<T> {
+        val response = client.get(route) {
+            url {
+                params.forEach { parameters.append(it.first, it.second) }
+            }
+        }
+        val value = response.body<Array<T>>()
+        return value
+    }
+
+    suspend fun getAllBoards(memberId: String) =
+        getList<TrelloBoard>(
+            "members/$memberId/boards",
+            "filter" to "open",
+            "fields" to "id,name",
+        )
+
+    suspend fun getAllListsInBoard(boardId: String) =
+        getList<TrelloList>("boards/$boardId/lists/open", "fields" to "id,name")
+
+    suspend fun getAllCardsInList(listId: String) =
+        getList<TrelloCard>("lists/$listId/cards")
 }
